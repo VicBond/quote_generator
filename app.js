5b17aff0b7154f4f411e5b302d42f6e35acb1904
Victor Bondarenko <nirasimha@gmail.com>
2022-05-20 15:28:48 +0000

    add event listeners to newQuoteBtn and twitterBtn

--- app.js
+++ app.js
@@ -43,5 +43,10 @@ function tweetQuote() {
   window.open(twitterUrl, '_blank');
 }
 
+//event listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote)
+
 //on load
 getQuotes();
