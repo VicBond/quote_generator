0c94eabdcb992b03c9aba3aff83cafa6392374cd
Victor Bondarenko <nirasimha@gmail.com>
2022-05-20 15:40:08 +0000

    add a loader

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
-
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// show loader
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+//hide loader
+function completeLoading() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+
 //new quote
 function newQuote() {
+  loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   //check if author field is empty
   if(!quote.author){
@@ -22,11 +35,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote');
   }
+  //set a quote and hide Loader
   quoteText.textContent = quote.text;
+  completeLoading();
 }
 
 // Get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
