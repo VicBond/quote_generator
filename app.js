c1af733652f681798399d8137ce0099aaaf39b35
Victor Bondarenko <nirasimha@gmail.com>
2022-05-20 15:02:59 +0000

    add new quote func

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let apiQuotes = [];
+
+//new quote
+function newQuote() {
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+}
+
 // Get quotes from API
 async function getQuotes() {
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    console.log(apiQuotes);
+    newQuote();
   } catch(err) {
     // catch error
   }
